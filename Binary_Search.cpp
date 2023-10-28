5f3b461fffb72956ca22fed2e19b02baabe82f21
CS-parth <parth.vijy@gmail.com>
2023-10-28 16:25:31 +0530

    fixed

--- Binary_Search.cpp
+++ Binary_Search.cpp
@@ -39,8 +39,29 @@ int binary_search__(vector<int> &nums,int key,int left,int right){
         return binary_search__(nums,key,left,right); // recursive call
     }
 }
+bool predicate(int mid){
+    return true;
+}
+int BS(int index){
+    int lo = -1;    // set lo to the first 0
+    int hi = index + 1; // set hi to the last possible 1
+    while(hi > lo + 1){
+        int mid = (hi - lo)/2 + lo;
+        if(predicate(mid)){
+            hi = mid;
+        }else{
+            lo = mid;
+        }
+    }
+    if(1){ // If you want to return the fist 1 than return hi
+        return hi;
+    }else{ // if you want to return last zero return lo
+        return lo;
+    }
+}
 int main(){
     vector<int> nums = {5,10,10,98,66,40,33,29};    // 5,10,10,29,33,40,66,98 //0-7
     sort(nums.begin(),nums.end());
     cout << binary_search__(nums,10,0,nums.size()-1);
 }
+
